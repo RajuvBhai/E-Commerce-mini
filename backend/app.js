31a3123eaaccc2ec1640db2db47fc1bbd4b19650
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-12-21 18:22:18 +0530

    Testing API routes with Thunderclind

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
 
+const products = require('./routes/productRouter');
+const order = require('./routes/orderRouter');
+
+// Prefix URL path
+app.use('/api/v1', products);
+app.use('/api/v1', order);
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
 })
