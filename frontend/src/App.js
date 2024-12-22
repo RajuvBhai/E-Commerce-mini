73ae9f6e23f236adb8ba0e3336377da6e3edc22d
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-12-22 18:11:11 +0530

    Creating Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div classNameName="App">
+      <Home/>
     </div>
   );
 }
