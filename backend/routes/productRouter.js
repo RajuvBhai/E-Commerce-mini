c8a44e643d83b3e923996ad0ce9a6ac62ee6da21
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-12-22 10:15:27 +0530

    missed packg nodemon

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -3,6 +3,6 @@ const { getProducts, getSingleProducts } = require('../controllers/productContro
 const router = express.Router();
 
 router.route('/products').get(getProducts);
-router.route('/products/:id').get(getSingleProducts);
+router.route('/product/:id').get(getSingleProducts);
 
 module.exports = router;
