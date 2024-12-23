30a252f8536b66bc219469eecb492677042579b2
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-12-23 09:48:02 +0530

    Fixing CORS error and i cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
+const cors = require('cors');
 const connectDatabase = require('./config/connectDatabase');
 dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
 
@@ -10,7 +11,8 @@ const order = require('./routes/orderRouter');
 
 connectDatabase();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 // Prefix URL path
 app.use('/api/v1', products);
 app.use('/api/v1', order);
